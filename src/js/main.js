0618191fe929c57c25112f98ae68546f2e02a582
chicano85 <chicano.cano@gmail.com>
2021-01-22 15:02:14 +0100

    add function play

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const btnReset = document.querySelector(".js-reset");
+
+function play(obj) {
+  let machine;
+  let res;
+
+  let objects = ["piedra", "papel", "tijera"];
+  machine = Math.floor(Math.random() * 3);
+
+  let movHuman = objects[obj];
+  let movMachine = objects[machine];
+
+  document.getElementById("human").innerHTML = objects[obj];
+  document.getElementById("machine").innerHTML = objects[machine];
+}
